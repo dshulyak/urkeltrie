8ac373225b0e161ea7a3a9ebc410b990e78f1db8
dmitry <yashulyak@gmail.com>
2020-01-06 15:48:34 +0200

    Extend fuzzer with proofs, updates and iterations
    Signed-off-by: dmitry <yashulyak@gmail.com>

--- fuzz_test.go
+++ fuzz_test.go
@@ -53,10 +53,69 @@ func (d *del) Run(tree *Tree) {
 	d.state.delete(d.key)
 }
 
-func genUpdate(tb testing.TB, state *state) testOp {
+func genInsert(tb testing.TB, state *state) testOp {
 	key := make([]byte, 10)
 	value := make([]byte, 50)
 	rand.Read(key)
+	rand.Read(value)
+	return &insert{
+		tb:    tb,
+		state: state,
+		key:   key,
+		value: value,
+	}
+}
+
+func genIterate(tb testing.TB, state *state) testOp {
+	return &iterate{
+		tb: tb, state: state,
+	}
+}
+
+type iterate struct {
+	tb    testing.TB
+	state *state
+}
+
+func (i *iterate) String() string {
+	return "Iterate"
+}
+
+func (i *iterate) Run(tree *Tree) {
+	cnt := 0
+	err := tree.Iterate(func(e Entry) bool {
+		key, err := e.Key()
+		require.NoError(i.tb, err)
+		val, err := e.Value()
+		require.NoError(i.tb, err)
+		cnt++
+
+		expected := i.state.get(key)
+		require.Equal(i.tb, expected, val)
+		return false
+	})
+	require.NoError(i.tb, err)
+	require.Equal(i.tb, i.state.entriesLth(), cnt)
+}
+
+type insert struct {
+	tb         testing.TB
+	state      *state
+	key, value []byte
+}
+
+func (u *insert) String() string {
+	return fmt.Sprintf("Insert %x = %x", u.key, u.value[:20])
+}
+
+func (u *insert) Run(tree *Tree) {
+	require.NoError(u.tb, tree.Put(u.key, u.value))
+	u.state.put(u.key, u.value)
+}
+
+func genUpdate(tb testing.TB, state *state) testOp {
+	key := state.randEntry()
+	value := make([]byte, 50)
 	rand.Read(value)
 	return &update{
 		tb:    tb,
@@ -83,13 +142,63 @@ func (u *update) Run(tree *Tree) {
 
 func genGet(tb testing.TB, state *state) testOp {
 	key := state.randEntry()
-	if key == nil {
+	if len(key) == 0 {
 		return new(noop)
 	}
 	return &get{
 		tb:    tb,
 		state: state,
 		key:   key,
+	}
+}
+
+func genMember(tb testing.TB, state *state) testOp {
+	key := state.randEntry()
+	if len(key) == 0 {
+		return new(noop)
+	}
+	return &proof{
+		member: true,
+		key:    key,
+		tb:     tb,
+		state:  state,
+	}
+}
+
+func genNotMember(tb testing.TB, state *state) testOp {
+	key := state.randDeleted()
+	if key == nil {
+		return new(noop)
+	}
+	return &proof{
+		key:   key,
+		tb:    tb,
+		state: state,
+	}
+}
+
+type proof struct {
+	member bool
+	key    []byte
+	tb     testing.TB
+	state  *state
+}
+
+func (p *proof) String() string {
+	if p.member {
+		return fmt.Sprintf("member %x", p.key)
+	}
+	return fmt.Sprintf("not member %x", p.key)
+}
+
+func (p *proof) Run(tree *Tree) {
+	proof := NewProof(0)
+	err := tree.GenerateProof(p.key, proof)
+	require.NoError(p.tb, err)
+	if p.member {
+		require.True(p.tb, proof.VerifyMembership(tree.Hash(), p.key))
+	} else {
+		require.True(p.tb, proof.VerifyNonMembership(tree.Hash(), p.key))
 	}
 }
 
@@ -165,6 +274,10 @@ type state struct {
 	deleted map[string]struct{}
 }
 
+func (s *state) entriesLth() int {
+	return len(s.entries)
+}
+
 func (s *state) put(key, value []byte) {
 	s.entries[string(key)] = value
 	delete(s.deleted, string(key))
@@ -208,18 +321,26 @@ func (s *state) randDeleted() []byte {
 }
 
 func TestFuzzTree(t *testing.T) {
+	if testing.Short() {
+		t.Skip("fuzz is skipped")
+		return
+	}
 	seed := time.Now().UnixNano()
 	rand.Seed(seed)
 	t.Logf("fuzz using seed: %d", seed)
 
 	// TODO add weights
-	gens := []genOp{genUpdate, genGet, genNotfound, genDel, genCommit}
+	gens := []genOp{
+		genUpdate, genInsert, genIterate,
+		genGet, genNotfound, genCommit,
+		genMember, genNotMember, genDel,
+	}
 	state := newState()
 
 	tree, closer := setupFullTreeP(t, 0)
 	defer closer()
 
-	for i := 0; i < 1000; i++ {
+	for i := 0; i < 10000; i++ {
 		index := rand.Intn(len(gens))
 		op := gens[index](t, state)
 		op.Run(tree)
